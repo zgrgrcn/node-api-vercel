2163c7279b4cafc200fc295b2ba4dd39778461da
ozgurgrcn <ozgur.gurcan@getir.com>
2023-11-04 17:16:49 +0300

    port changed to 5000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 const { client, marketOrder, limitOrder } = require('./binance/binance');
